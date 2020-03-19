3d0e8276d4b09dc92ef8ab9777ea1beee761a420
Will Thomas <30wthomas@gmail.com>
2020-03-19 18:05:05 -0500

    Daily commit and update to site

---
+++ script.js
@@ -0,0 +1,7 @@
+const goProjects = () => {
+    document.getElementById("leftSideBar").style.background = "aqua";
+};
+
+const goPow = () => {
+    document.getElementById("contentContainer").style.backgroundColor = "aqua";
+};
