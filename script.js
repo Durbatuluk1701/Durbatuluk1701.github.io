0d320ec17c226e00db4db35d67c823604be89268
Will Thomas <30wthomas@gmail.com>
2020-03-21 15:06:10 -0500

    Bluring page

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ const goProjects = () => {
 const goPow = () => {
     document.getElementById("contentContainer").style.backgroundColor = "aqua";
 };
+
+var blurMessage = [
+    "Come Back!!!",
+    "WHY HAVE YOU FORSAKEN ME",
+    "Fine I didnt need you anyways",
+    "PLEASE PLEASE PLEASE",
+    "Fine"
+];
+
+window.addEventListener("blur", () => {
+    var rand = Math.floor((Math.random() * 5));
+    document.title = blurMessage[rand];
+})
+
+window.addEventListener("focus", () => {
+    document.title = "Will Thomas";
+})
