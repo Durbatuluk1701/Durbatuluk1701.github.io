6844b6d5ea8ac93bcca105b502416bc2efb4ff5d
Will Thomas <30wthomas@gmail.com>
2020-03-29 18:19:32 -0500

    Web Updates

--- script.js
+++ script.js
@@ -5,15 +5,44 @@ const displayHome = () => {
     `;
 };
 
+const displayProjects = () => {
+    document.getElementById("contentContainer").innerHTML = `
+    <h1>My Favorite Projects</h1>
+    `;
+};
+
+const displaySkills = () => {
+    document.getElementById("contentContainer").innerHTML = `
+    <h1>Skills</h1>
+    <table style='width: 100%'>
+        <tr>
+            <th>Language</th>
+            <th>Expertise</th>
+        </tr>
+        <tr>
+            <td>C#</td>
+            <td>45%</td>
+        </tr>
+    </table>
+    `;
+};
+
+const displayContact = () => {
+    document.getElementById("contentContainer").innerHTML = `
+    <h1 style="text-decoration: underline; color: red;">RED ALERT</h1>
+    <p style="margin-top: .5em">This section is still <b>in-progress</b>, 
+    please contact me directly through my email or by my LinkedIn
+    </p>
+    `;
+}
+
 setInterval(() => {
     let likeArray = [
         "CODE",
-        "DANCE",
-        "SING",
         "ANALYZE",
         "PROGRAM",
-        "BAKE",
-        "COOK"
+        "LEARN",
+        "ADAPT"
     ]
     let rand = Math.floor(Math.random()*likeArray.length);
     document.getElementById("randomLike").innerHTML = `${likeArray[rand]}`;
