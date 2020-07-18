1fb75cb361aaf75c03a2ab8613a2aba77db67a4f
Will Thomas <30wthomas@gmail.com>
2020-07-18 18:35:04 -0500

    Updates

---
+++ precache-manifest.8067ee42f742a41079327b94d5433623.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe81123e5c955e0eddee9fad3cac5f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c8d4ca4f1b5ba62bd25",
+    "url": "/static/js/2.f3be9db8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f3be9db8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2aa4ff23cb714318a39",
+    "url": "/static/js/main.ae120f85.chunk.js"
+  },
+  {
+    "revision": "db3a52397273d545b379",
+    "url": "/static/js/runtime-main.28675e69.js"
+  }
+]);
